5438f37935a5059a201318b0258f7e220dcc0761
bvreeman <bvreeman@gmail.com>
2018-03-26 00:05:18 -0500

    got the gifs to add and got the click function to work

--- assets/scripts/giphy.js
+++ assets/scripts/giphy.js
@@ -3,27 +3,68 @@
 
 const motivation = ['Inspiration', 'Happiness', 'Strength', 'Motivate', 'Inspire', 'Happy', 'Positive', 'Greatness', 'You are good', 'Perfect', 'Inspired'];
 // const URL = 'https://api.giphy.com/v1/gifs/search?q&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA';
-const funnyCat = '"http://api.giphy.com/v1/gifs/search?q=funny+cat.type&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA"';
+// const funnyCat = '"http://api.giphy.com/v1/gifs/search?q=funny+cat.type&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA"';
 // const URL = 'https://api.giphy.com/v1/gifs/search?';
 // const apiKey = '&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA';
-const motivate = $(this).attr('data-name');
 // const query = `"&q="${motivate}`;
 
-function displayGiphy() {
-  const queryURL = `"https://api.giphy.com/v1/gifs/search?&q="${motivate}"&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA&limit=10"`;
+function onHover() {
 
+}
+
+function displayRating(event) {
+  const motivate = $(event.target).data('name');
+  // const motivate = $(this).attr('data-name');
+  const queryURL = `https://api.giphy.com/v1/gifs/search?&q=${motivate}&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA&limit=10`;
+  // console.log(motivate);
   $.ajax({
     url: queryURL,
     method: 'GET',
   }).then(function(response) {
-    const motivationDiv = $("<div class='motivate'>");
-    const rating = response.data[0].rating;
-    const pOne = ('<p>').text(`Rating: ${rating}`);
-    motivationDiv.append(pOne);
+    // const motivationRatingDiv = $("<div class='rating'>");
+    for (let i = 0; i < motivation.length; i++) {
+      const giphyRating = $(`<p style="font-size: 20px;">Rating: ${response.data[i].rating}</p>`);
+      const giphyImages = $('<img>');
+      giphyImages.attr('src', response.data[i].images.fixed_height_still.url);
+      giphyImages.attr('data-still', response.data[i].images.fixed_height_still.url);
+      giphyImages.attr('data-animate', response.data[i].images.fixed_height.url);
+      giphyImages.attr('data-state', 'still');
+      giphyImages.addClass('image');
+      $('#giphy').append(giphyRating, giphyImages);
+    }
 
-    $('#giphys').append(motivationDiv);
+    // const ratings = response.data[i].rating;
+    // const pOne = $('<p>').text(`Rating: ${ratings}`);
+    // motivationRatingDiv.append(pOne);
+    // $('#giphyRating').append(motivationRatingDiv);
+
+    // const motivationGiphyDiv = $("<div class='motivation'>");
+    // const giphyPic = response.data[i].images.original.url;
+    // const giphy = $('<img>').html(`${giphyPic}`);
+    // motivationGiphyDiv.append(giphy);
+    // $('#giphy').append(motivationGiphyDiv);
+    // const motivationGiphyDiv = `<div style="float: left;"><p style="font-size: 20px;">
+    // Rating: ${response.data[i].rating}</p><img class="gif"
+    // src="${response.data[i].images.fixed_height_still.url}"/></div>`;
+
+    // $('#giphy').append(motivationGiphyDiv);
+
+    // src="${response.data[i].images.fixed_height_still.url},
+    // }
   });
 }
+
+
+$(document).on('click', '.image', function() {
+  const imageState = $(this).attr('data-state');
+  if (imageState == 'still') {
+    $(this).attr('src', $(this).data('animate'));
+    $(this).attr('data-state', 'animate');
+  } else {
+    $(this).attr('src', $(this).data('still'));
+    $(this).attr('data-state', 'still');
+  }
+});
 
 function renderButtons() {
   $('#giphyButtons').empty();
@@ -45,9 +86,26 @@ $('#moreMotivation').on('click', function(event) {
   renderButtons();
 });
 
-$(document).on('click', '.motivate-btn', displayGiphy);
+$('#giphyButtons').on('click', function(event) {
+  event.preventDefault();
+  $('#giphy').empty();
+});
+
+$('#stopGiphy').on('click', function(event) {
+  event.preventDefault();
+  $('#giphy').empty();
+});
+
+$(document).on('click', '.motivate-btn', displayRating);
 
 renderButtons();
+
+// const motivationDiv = $("<div class='motivate'>");
+// for (let j = 0; j < motivation.length; j++) {
+//   const giphyPic = response.data[j].url;
+//   const giphy = $('<img>').append(`${giphyPic}`);
+//   motivationDiv.append(giphy);
+//   $('#giphys').append(motivationDiv);
 
 // const giphyURLWithAPIKey = 'https://api.giphy.com/GET/v1/gifs/search?&api_key=sd0BFqPzqnPbrf9aXtsAg1gqTitD94RA';
 
